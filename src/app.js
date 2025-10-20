f0abb8fe9ec20a05b11ce2731b3caca8c942eb77
José Isidro <isidro107109@gmail.com>
2025-10-20 20:20:42 +0000

    temporizador terminado

--- src/app.js
+++ src/app.js
@@ -7,49 +7,72 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function () {
   //write your code here
-  let numeros = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-  let simbolos = ['♦', '♥', '♠', '♣']
+  let numeros = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+  let simbolos = ['♦', '♥', '♠', '♣'];
   let div = document.getElementById('carta');
-  let simbolosRandom = simbolos[Math.floor(Math.random() * simbolos.length)]
-  let numeroRandom = numeros[Math.floor(Math.random() * numeros.length)];
+  let tiempo = 10
+  let intervalo;
 
+  let temporizador = document.createElement('h1');
+  document.body.appendChild(temporizador);
+
+  let button = document.getElementById('reset');
+  button.classList.add("btn", "btn-outline-light");
 
   let simbolo1 = document.createElement('h2')
-  simbolo1.innerHTML = simbolosRandom
-  Object.assign(simbolo1.style,{
-    alignItems: 'space-start',
-    fontSize: '120px',
-    display: 'flex',
-    marginLeft:'20px'
-  })
-  div.appendChild(simbolo1)
+  simbolo1.innerHTML = '♠';
+  simbolo1.classList.add("simbolo1");
+  div.appendChild(simbolo1);
 
   let numero = document.createElement('h1');
-  numero.innerHTML = numeroRandom
-  Object.assign(numero.style, {
-    alignItems: 'center',
-    fontSize: '150px',
-    display: 'flex',
-
-  })
+  numero.innerHTML = 'A';
+  numero.classList.add("numero");
   div.appendChild(numero);
 
-  let simbolo2 = document.createElement('h2')
-  simbolo2.innerHTML = simbolosRandom
-  Object.assign(simbolo2.style, {
-    fontSize: '120px',
-    alignItems: 'space-end',
-    display: 'flex',
-    transform: 'rotate(180deg)',
-    marginRight:'20px'
-  })
-  div.appendChild(simbolo2)
-  if (simbolosRandom == '♥' || simbolosRandom == '♦') {
-    simbolo1.style.color = '#ff2c2c';
-    simbolo2.style.color = '#ff2c2c';
-    numero.style.color = '#ff2c2c';
+  let simbolo2 = document.createElement('h2');
+  simbolo2.innerHTML = '♠';
+  simbolo2.classList.add("simbolo2");
+  div.appendChild(simbolo2);
 
+  function botonDeCarta() {
+    let simbolosRandom = simbolos[Math.floor(Math.random() * simbolos.length)]
+    let numeroRandom = numeros[Math.floor(Math.random() * numeros.length)];
+    simbolo1.innerHTML = simbolosRandom;
+    numero.innerHTML = numeroRandom;
+    simbolo2.innerHTML = simbolosRandom;
+
+    if (simbolosRandom == '♥' || simbolosRandom == '♦') {
+      simbolo1.style.color = '#ff2c2c';
+      simbolo2.style.color = '#ff2c2c';
+      numero.style.color = '#ff2c2c';
+    } else {
+      simbolo1.style.color = '#000000';
+      simbolo2.style.color = '#000000';
+      numero.style.color = '#000000';
+    }
   }
 
+  
 
+  function cuentraregresiva() {
+    intervalo = setInterval(() => {
+      temporizador.innerHTML = tiempo
+      tiempo--;
+
+      if (tiempo === -1) {
+        botonDeCarta()
+        clearInterval(intervalo);
+        tiempo = 10;
+        cuentraregresiva()
+      }
+    }, 1000);
+  }
+  cuentraregresiva()
+  button.addEventListener('click', function(){
+    clearInterval(intervalo);
+    tiempo = 10;
+    cuentraregresiva();
+    botonDeCarta();
+  });
+  
 };
